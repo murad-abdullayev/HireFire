1bd1467193553977d4aa5186c5b75f5f19d519ff
murad-abdullayev <muradsab@code.edu.az>
2025-01-17 23:00:05 +0400

    feat(frontend): update fonts, container, title, and favicon

--- client/src/pages/(user)/(common)/landing/index.tsx
+++ client/src/pages/(user)/(common)/landing/index.tsx
@@ -1,5 +1,13 @@
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  return (
+    <div className="container">
+      <h1 className="font-semibold text-[72px]">Typography</h1>
+      <p className="text-xl font-semibold text-[#515B6F]">
+        Epilogue - Body , Button , Display
+      </p>
+      <button>Hello</button>
+    </div>
+  );
 };
 
 export default LandingPage;
