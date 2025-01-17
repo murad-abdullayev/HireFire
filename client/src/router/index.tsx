dae9591a5bb3e88d30bd0ea4d1387744785233bc
murad-abdullayev <muradsab@code.edu.az>
2025-01-17 21:02:03 +0400

    feat(frontend): integrate TailwindCSS and Shadcn for UI styling

--- client/src/router/index.tsx
+++ client/src/router/index.tsx
@@ -1,4 +1,4 @@
-// import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 // import RootLayout from "./components/shared/RootLayout";
 // import AuthLayout from "./components/shared/AuthLayout";
 // import ApplicantDashboardLayout from "./components/shared/ApplicantDashboardLayout";
@@ -23,85 +23,90 @@
 // import CompanyJobListingPage from "./pages/company/JobListing";
 // // Admin Pages
 // import AdminDashboardPage from "./pages/admin/Dashboard";
-// import { adminPaths, paths } from "../constants/paths";
+import { paths } from "../constants/paths";
+import LandingPage from "../pages/(user)/(common)/landing";
 
-// export const router = createBrowserRouter([
-//   {
-//     path: paths.LANDING.MAIN,
-//     element: <RootLayout />,
-//     children: [
-//       { path: paths.LANDING.MAIN, element: <LandingPage /> },
-//       { path: paths.LANDING.COMPANIES.MAIN, element: <CompaniesPage /> },
-//       {
-//         path: paths.LANDING.COMPANIES.DETAILS(),
-//         element: <CompanyDetailsPage />,
-//       },
-//       { path: paths.LANDING.JOBS.MAIN, element: <JobsPage /> },
-//       { path: paths.LANDING.JOBS.DETAILS(), element: <JobDetailsPage /> },
-//     ],
-//   },
-//   {
-//     path: paths.AUTH.JOBSEEKER.REGISTER,
-//     element: <AuthLayout />,
-//     children: [
-//       {
-//         path: paths.AUTH.JOBSEEKER.REGISTER,
-//         element: <div>Register Page</div>,
-//       },
-//       { path: paths.AUTH.JOBSEEKER.LOGIN, element: <div>Login Page</div> },
-//     ],
-//   },
-//   {
-//     path: paths.APPLICANT_DASHBOARD.MAIN,
-//     element: <ApplicantDashboardLayout />,
-//     children: [
-//       { path: paths.APPLICANT_DASHBOARD.CHAT, element: <ApplicantChatPage /> },
-//       {
-//         path: paths.APPLICANT_DASHBOARD.APPLICATIONS,
-//         element: <ApplicantApplicationsPage />,
-//       },
-//       {
-//         path: paths.APPLICANT_DASHBOARD.JOBS.MAIN,
-//         element: <ApplicantJobsPage />,
-//       },
-//       {
-//         path: paths.APPLICANT_DASHBOARD.COMPANIES.MAIN,
-//         element: <ApplicantCompaniesPage />,
-//       },
-//       {
-//         path: paths.APPLICANT_DASHBOARD.PROFILE,
-//         element: <ApplicantProfilePage />,
-//       },
-//     ],
-//   },
-//   {
-//     path: paths.COMPANY_DASHBOARD.MAIN,
-//     element: <CompanyDashboardLayout />,
-//     children: [
-//       { path: paths.COMPANY_DASHBOARD.CHAT, element: <CompanyChatPage /> },
-//       {
-//         path: paths.COMPANY_DASHBOARD.PROFILE,
-//         element: <CompanyProfilePage />,
-//       },
-//       {
-//         path: paths.COMPANY_DASHBOARD.APPLICANTS.MAIN,
-//         element: <CompanyApplicantsPage />,
-//       },
-//       {
-//         path: paths.COMPANY_DASHBOARD.SCHEDULE,
-//         element: <CompanySchedulePage />,
-//       },
-//       {
-//         path: paths.COMPANY_DASHBOARD.JOB_LISTING.MAIN,
-//         element: <CompanyJobListingPage />,
-//       },
-//     ],
-//   },
-//   {
-//     path: adminPaths.DASHBOARD.MAIN,
-//     element: <AdminDashboardLayout />,
-//     children: [
-//       { path: adminPaths.DASHBOARD.MAIN, element: <AdminDashboardPage /> },
-//     ],
-//   },
-// ]);
+export const router = createBrowserRouter([
+  {
+    path: paths.LANDING.MAIN,
+    element: <LandingPage />,
+  },
+  //   {
+  //     path: paths.LANDING.MAIN,
+  //     element: <RootLayout />,
+  //     children: [
+  //       { path: paths.LANDING.MAIN, element: <LandingPage /> },
+  //       { path: paths.LANDING.COMPANIES.MAIN, element: <CompaniesPage /> },
+  //       {
+  //         path: paths.LANDING.COMPANIES.DETAILS(),
+  //         element: <CompanyDetailsPage />,
+  //       },
+  //       { path: paths.LANDING.JOBS.MAIN, element: <JobsPage /> },
+  //       { path: paths.LANDING.JOBS.DETAILS(), element: <JobDetailsPage /> },
+  //     ],
+  //   },
+  //   {
+  //     path: paths.AUTH.JOBSEEKER.REGISTER,
+  //     element: <AuthLayout />,
+  //     children: [
+  //       {
+  //         path: paths.AUTH.JOBSEEKER.REGISTER,
+  //         element: <div>Register Page</div>,
+  //       },
+  //       { path: paths.AUTH.JOBSEEKER.LOGIN, element: <div>Login Page</div> },
+  //     ],
+  //   },
+  //   {
+  //     path: paths.APPLICANT_DASHBOARD.MAIN,
+  //     element: <ApplicantDashboardLayout />,
+  //     children: [
+  //       { path: paths.APPLICANT_DASHBOARD.CHAT, element: <ApplicantChatPage /> },
+  //       {
+  //         path: paths.APPLICANT_DASHBOARD.APPLICATIONS,
+  //         element: <ApplicantApplicationsPage />,
+  //       },
+  //       {
+  //         path: paths.APPLICANT_DASHBOARD.JOBS.MAIN,
+  //         element: <ApplicantJobsPage />,
+  //       },
+  //       {
+  //         path: paths.APPLICANT_DASHBOARD.COMPANIES.MAIN,
+  //         element: <ApplicantCompaniesPage />,
+  //       },
+  //       {
+  //         path: paths.APPLICANT_DASHBOARD.PROFILE,
+  //         element: <ApplicantProfilePage />,
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     path: paths.COMPANY_DASHBOARD.MAIN,
+  //     element: <CompanyDashboardLayout />,
+  //     children: [
+  //       { path: paths.COMPANY_DASHBOARD.CHAT, element: <CompanyChatPage /> },
+  //       {
+  //         path: paths.COMPANY_DASHBOARD.PROFILE,
+  //         element: <CompanyProfilePage />,
+  //       },
+  //       {
+  //         path: paths.COMPANY_DASHBOARD.APPLICANTS.MAIN,
+  //         element: <CompanyApplicantsPage />,
+  //       },
+  //       {
+  //         path: paths.COMPANY_DASHBOARD.SCHEDULE,
+  //         element: <CompanySchedulePage />,
+  //       },
+  //       {
+  //         path: paths.COMPANY_DASHBOARD.JOB_LISTING.MAIN,
+  //         element: <CompanyJobListingPage />,
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     path: adminPaths.DASHBOARD.MAIN,
+  //     element: <AdminDashboardLayout />,
+  //     children: [
+  //       { path: adminPaths.DASHBOARD.MAIN, element: <AdminDashboardPage /> },
+  //     ],
+  //   },
+]);
