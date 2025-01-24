2935315e6d4639b983398f2d7623d0298ce6cb4c
murad-abdullayev <muradsab@code.edu.az>
2025-01-24 19:27:39 +0400

    Merge branch 'frontend' into homepage

--- client/src/components/navbar/index.tsx
+++ client/src/components/navbar/index.tsx
@@ -26,28 +26,51 @@ export const Navbar = () => {
   }, [controlNavbar]);
 
   return (
-    <div className="bg-white py-[14px] flex items-center justify-between">
-      <div className="flex items-center gap-12">
-        <Logo />
-        <ul className="flex gap-4 items-center">
-          <Link
-            to={paths.LANDING.JOBS.MAIN}
-            className="text-neutrals80 text-base font-medium leading-[160%] hover:text-neutrals100 transition"
+    <>
+      <div
+        className={`bg-white fixed w-full z-50 transition-transform duration-300 ${
+          isVisible ? "translate-y-0" : "-translate-y-full"
+        }`}
+      >
+        <div className="container py-[14px] flex items-center justify-between">
+          <div className="flex items-center gap-12">
+            <Logo />
+            <ul className="hidden sm:flex gap-8 items-center relative">
+              <li className="relative group">
+                <Link
+                  to={paths.LANDING.JOBS.MAIN}
+                  className="block px-4 py-2 text-neutrals80 text-sm md:text-base font-medium leading-[160%] group-hover:text-primary transition"
+                >
+                  Find Jobs
+                </Link>
+                <div className="absolute left-0 bottom-[-13.5px] lg:bottom-[-17.5px] h-[2px] w-full bg-primary scale-x-0 group-hover:scale-x-100 origin-left transition-transform duration-300" />
+              </li>
+              <li className="relative group">
+                <Link
+                  to={paths.LANDING.COMPANIES.MAIN}
+                  className="block px-4 py-2 text-neutrals80 text-sm md:text-base font-medium leading-[160%] group-hover:text-primary transition"
+                >
+                  Browse Companies
+                </Link>
+                <div className="absolute left-0 bottom-[-13.5px] lg:bottom-[-17.5px] h-[2px] w-full bg-primary scale-x-0 group-hover:scale-x-100 origin-left transition-transform duration-300" />
+              </li>
+            </ul>
+          </div>
+          <div className="items-center gap-2 md:gap-4 hidden sm:flex">
+            <Button className="text-sm md:text-base" variant={"ghost"}>
+              Login
+            </Button>
+            <span className="h-12 w-[2px] bg-neutrals20" />
+            <Button className="text-sm md:text-base">Sign Up</Button>
+          </div>
+          <Button
+            size={"icon"}
+            className="bg-white sm:hidden rounded-full flex items-center hover:bg-neutrals20 border border-neutrals20"
+            variant={"secondary"}
           >
-            Find Jobs
-          </Link>
-          <Link
-            to={paths.LANDING.COMPANIES.MAIN}
-            className="text-neutrals80 text-base font-medium leading-[160%] hover:text-neutrals100 transition"
-          >
-            Browse Companies
-          </Link>
-        </ul>
-      </div>
-      <div className="flex items-center gap-4">
-        <Button variant={"ghost"}>Login</Button>
-        <span className="h-12 w-[2px] bg-neutrals20" />
-        <Button>Sign Up</Button>
+            <img src={BurgerIcon} alt="burger" />
+          </Button>
+        </div>
       </div>
       <div className="h-[64px] sm:h-[76px]" />
     </>
